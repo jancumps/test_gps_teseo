7efe6235167b4e61b425d05e9bab14365ae1a89a
Jan Cumps <jcumps@gmail.com>
2025-02-24 13:20:35 -0500

    add more teseo tests

--- test/gps_teseo_lib/test_gps_teseo_lib.cpp
+++ test/gps_teseo_lib/test_gps_teseo_lib.cpp
@@ -28,11 +28,25 @@ protected:
     unsigned int count;
 };
 
-TEST_F(teseoTest, parse_multiline_reply) {
+TEST_F(teseoTest, parseMultilineReply) {
     teseo::nmea_rr command("testcommand", "TST");
     reply = "XXXTST\r\nXXXTST\r\ntestcommand\r\n";
     EXPECT_TRUE(test_parse_multiline_reply(command));
     EXPECT_EQ(count, 2);
     EXPECT_STREQ(replies[0].c_str(), "XXXTST\r\n");
     EXPECT_STREQ(replies[1].c_str(), "XXXTST\r\n");
+}
+
+TEST_F(teseoTest, parseSinglelineReply) {
+    teseo::nmea_rr command("testcommand", "TST");
+    reply = "XXXTST\r\ntestcommand\r\n";
+    EXPECT_TRUE(test_parse_multiline_reply(command));
+    EXPECT_EQ(count, 1);
+    EXPECT_STREQ(replies[0].c_str(), "XXXTST\r\n");
+}
+
+TEST_F(teseoTest, callbacksSet) {
+    EXPECT_TRUE(o.writer().is_set());
+    EXPECT_TRUE(o.reader().is_set());
+    EXPECT_TRUE(o.resetter().is_set());
 }
